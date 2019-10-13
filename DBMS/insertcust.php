2b527a8b89872fbec99bf94df44beaf9173c7bd8
dumb-coder007 <55644971+dumb-coder007@users.noreply.github.com>
2019-10-13 18:41:39 +0530

    insetcust

---
+++ DBMS/insertcust.php
@@ -0,0 +1,30 @@
+<?php
+
+$custname=filter_input(INPUT_POST, 'n');
+$custnum=filter_input(INPUT_POST, 'no');
+
+if(!empty($custname)){
+if(!empty($custnum)){
+
+$conn=new mysql_connect("localhost","root","","restaurant");
+
+$sql="INSERT INTO customer (CNAME,CNUMBER) VALUES ('$custname','$custnum')";
+
+if($conn->query($sql)){
+	echo "New record inserted";
+}
+else{
+	echo "record not inserted";
+}
+$conn->close();
+}
+
+else{
+    echo "Customer number should not be empty ";
+    die();
+}}
+else{
+	echo "Customer name should not be empty";
+	die();
+}
+?>
