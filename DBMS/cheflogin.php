3a83d685cd55b3c2674740dc2f44a31734fb9f51
dumb-coder007 <55644971+dumb-coder007@users.noreply.github.com>
2019-10-14 20:49:30 +0530

    updated menu

---
+++ DBMS/cheflogin.php
@@ -0,0 +1,63 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title> RESTAURANT MANAGEMENT </title>
+		<meta charset="UTF-8">
+		<link rel= stylesheet href="main.css">
+	</head>
+	<body>
+		<nav class= "navbar">
+			<div>
+				<h2>LOGO</h2>
+				<ul class="nav-ul">
+					<a href="page.html">Home</a>
+					<a href="items.html">Menu</a>
+					<a href ="#">Contact Us</a>
+				</ul>
+			</div>
+		</nav>
+		
+		<div class="container">
+			<table>
+      <tr>
+        <th>OID</th>
+        <th>QUANTITY</th>
+        <th>TOTAL</th>
+        <th>NAME</th>
+        <th>CID</th>
+        <th>WID</th>
+        <th>ID</th>
+      </tr>
+      
+      <?php
+
+      $conn= mysqli_connect("localhost","root","","restaurant");
+      
+      $sql= "SELECT ORDERID,QUANTITY,TOTAL,NAME,CID,WID,ID FROM orders";
+      $result=$conn->query($sql);
+
+      if($result->num_rows>0){
+      while($row=$result-> fetch_assoc()){
+
+      echo "<tr><td>".$row["ORDERID"]."</td><td>".$row["QUANTITY"]."</td><td>".$row["TOTAL"]."</td><td>".$row["NAME"]."</td><td>".$row["CID"]."</td><td>".$row["WID"]."</td><td>".$row["ID"]."</td></tr>>";
+		}
+
+	echo "</table>";
+		}
+		$conn->close();
+		
+		?>
+      
+      </table>
+		</div>
+		<div class="footer">
+				<h2>LOGO</h2>
+				
+				<ul class="foot-ul">
+						<a href="#">Facebook</a>
+						<a href="#">Zomato</a>
+						<a href="#">Swiggy</a>
+				</ul>
+			</div>
+	</body>
+</html>
--- DBMS/restaurant.sql
+++ DBMS/restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2019 at 07:40 AM
+-- Generation Time: Oct 14, 2019 at 05:18 PM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.2.19
 
@@ -65,6 +65,29 @@ INSERT INTO `items` (`ID`, `CATEGORY`, `NAME`, `PRICE`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ORDERID` int(11) NOT NULL,
+  `QUANTITY` int(11) NOT NULL,
+  `TOTAL` int(11) NOT NULL,
+  `NAME` varchar(20) NOT NULL,
+  `CID` int(11) DEFAULT NULL,
+  `WID` int(11) DEFAULT NULL,
+  `ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`ORDERID`, `QUANTITY`, `TOTAL`, `NAME`, `CID`, `WID`, `ID`) VALUES
+(1, 2, 250, 'idli', 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `waiter`
 --
 
@@ -99,6 +122,15 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`ORDERID`),
+  ADD KEY `CID` (`CID`),
+  ADD KEY `WID` (`WID`),
+  ADD KEY `ID` (`ID`);
+
+--
 -- Indexes for table `waiter`
 --
 ALTER TABLE `waiter`
@@ -125,6 +157,18 @@ ALTER TABLE `items`
 --
 ALTER TABLE `waiter`
   MODIFY `WID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`CID`) REFERENCES `customer` (`CID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`WID`) REFERENCES `waiter` (`WID`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`ID`) REFERENCES `items` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
