8aa9f19de53f274f66951bfa04c5be71dee6cefe
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-10-18 12:59:58 +0530

    updated

--- DBMS/restaurant.sql
+++ DBMS/restaurant.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2019 at 09:23 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.1.32
+-- Generation Time: Oct 18, 2019 at 09:28 AM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.1.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,9 +30,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `bill` (
   `BID` int(11) NOT NULL,
-  `CID` int(11) DEFAULT NULL,
-  `WID` int(11) DEFAULT NULL
+  `TOTAL` int(11) NOT NULL,
+  `TABLENO` varchar(50) COLLATE utf16_bin NOT NULL,
+  `DATE` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`BID`, `TOTAL`, `TABLENO`, `DATE`) VALUES
+(2, 500, '1', '2019-10-14'),
+(3, 155, '3', '2019-10-16'),
+(4, 50, '1', '2019-10-17'),
+(5, 200, '6', '2019-10-16');
 
 -- --------------------------------------------------------
 
@@ -75,7 +86,8 @@ INSERT INTO `customer` (`CID`, `CNAME`, `CNUMBER`) VALUES
 (2, 'Aditya', '9876543211'),
 (3, 'Aditya', '9876543211'),
 (4, 'akshay', '1234'),
-(5, 'sad', '23');
+(5, 'sad', '23'),
+(6, 'Sunita Mam', '9876543210');
 
 -- --------------------------------------------------------
 
@@ -95,8 +107,13 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`ID`, `CATEGORY`, `NAME`, `PRICE`) VALUES
-(1, 'South Indian', 'Idli Sambhar', 45),
-(2, 'Italian', 'Mac n Cheese', 145);
+(1, 'SouthIndian', 'Idli ', 45),
+(2, 'Italian', 'Pasta', 145),
+(3, 'SouthIndian', 'Dosa', 200),
+(4, 'SouthIndian', 'Vada', 75),
+(5, 'Italian', 'Pizza', 375),
+(6, 'Chinese', 'Manchurian', 255),
+(7, 'Chinese', 'Soup', 155);
 
 -- --------------------------------------------------------
 
@@ -108,18 +125,9 @@ CREATE TABLE `orders` (
   `ORDERID` int(11) NOT NULL,
   `QUANTITY` int(11) NOT NULL,
   `TOTAL` int(11) NOT NULL,
-  `NAME` varchar(20) NOT NULL,
-  `CID` int(11) DEFAULT NULL,
-  `WID` int(11) DEFAULT NULL,
-  `ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`ORDERID`, `QUANTITY`, `TOTAL`, `NAME`, `CID`, `WID`, `ID`) VALUES
-(1, 2, 250, 'idli', 1, 1, 1);
+  `NAME` varchar(60) COLLATE utf16_bin NOT NULL,
+  `TABLENO` varchar(11) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 -- --------------------------------------------------------
 
@@ -150,9 +158,7 @@ INSERT INTO `waiter` (`WID`, `WNAME`, `WPASS`) VALUES
 -- Indexes for table `bill`
 --
 ALTER TABLE `bill`
-  ADD PRIMARY KEY (`BID`),
-  ADD KEY `fk_cid` (`CID`),
-  ADD KEY `fk_wid` (`WID`);
+  ADD PRIMARY KEY (`BID`);
 
 --
 -- Indexes for table `cashier`
@@ -176,10 +182,7 @@ ALTER TABLE `items`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`ORDERID`),
-  ADD KEY `CID` (`CID`),
-  ADD KEY `WID` (`WID`),
-  ADD KEY `ID` (`ID`);
+  ADD PRIMARY KEY (`ORDERID`);
 
 --
 -- Indexes for table `waiter`
@@ -195,7 +198,7 @@ ALTER TABLE `waiter`
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `BID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `BID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `cashier`
@@ -207,38 +210,25 @@ ALTER TABLE `cashier`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `ORDERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `waiter`
 --
 ALTER TABLE `waiter`
   MODIFY `WID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bill`
---
-ALTER TABLE `bill`
-  ADD CONSTRAINT `fk_cid` FOREIGN KEY (`CID`) REFERENCES `customer` (`CID`),
-  ADD CONSTRAINT `fk_wid` FOREIGN KEY (`WID`) REFERENCES `waiter` (`WID`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`CID`) REFERENCES `customer` (`CID`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`WID`) REFERENCES `waiter` (`WID`),
-  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`ID`) REFERENCES `items` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
